acbe76a11b0a209ae23458014ddf53ff9c6aed29
Hanneslol <hannes@hanneswallin.com>
2023-11-29 12:45:57 +0100

    await, async and axios

--- ovningpromise/ovning-promise/main.js
+++ ovningpromise/ovning-promise/main.js
@@ -69,7 +69,10 @@ button.addEventListener("click"() =>
   });
 
 .env API_KEY = 123123
+
   .env.development  API_KEY = 123123
+
+import.meta.env.VITE_API_KEY
 
 
 main.js dela upp i olika document: services, models, htmlCreator
@@ -88,6 +91,42 @@ serviceBase.js
 
 
 Presentation -> application -> data
+
+
+main.js
+
+movieService.js      service.Base
+
+
+
+main.js:
+
+document.getElementById("") "Click, async ()" => {
+
+  await searchMovies(input.value);
+  createHtml(m);
+}
+
+movieSearch.js
+
+const searchMovies = async (title) => {
+
+  const data = await get("...")
+  return []
+
+
+}
+
+
+
+serviceBase.js
+
+const get = async (url) => {
+  fetch(url)
+  const r = await fetch(url);
+  const data = await r.json;
+  return data.Search
+}
 
 
 
